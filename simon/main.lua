ae1543cd7aac8efbcdcb80d8d9a87933611171d3
Bruno Oliveira <86791361+Bruno514@users.noreply.github.com>
2024-05-10 14:32:50 -0300

    adiciona um botão de jogar

--- simon/main.lua
+++ simon/main.lua
@@ -1,4 +1,4 @@
-local colors = {"1" : red, "2" : green, "3" : blue, "4" : yellow}
+local colors = {"red", "green", "blue", "yellow"}
 
 local currentSequence = tostring(math.random(#colors))
 
@@ -15,11 +15,27 @@ end
 isPlaying = false
 
 function love.load()
-  playButton = {"x" = love.graphics.getWidth() / 2, "y" = love.graphics.getHeight(), "width" = 200, "height" = 100}
+  love.window.setTitle("Simon")
+  playBtnFont = love.graphics.setNewFont(20)
+  playBtn = {x = love.graphics.getWidth() / 2, y = love.graphics.getHeight() / 2, w = 200, h = 100}
+  playBtnText = "Jogar"
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.setBackgroundColor(20/255, 20/255, 20/255)
+  if isPlaying == false then
+    local fontHeight = playBtnFont:getHeight(playBtnText) -- font expected, got null
+    local fontWidth = playBtnFont:getWidth(playBtnText)
+
+    love.graphics.rectangle("fill", playBtn.x - (playBtn.w / 2), playBtn.y - (playBtn.h / 2), playBtn.w, playBtn.h, 20)
+    love.graphics.setFont(playBtnFont)
+    love.graphics.print({{0,0,0}, playBtnText}, playBtn.x - (fontWidth / 2), playBtn.y - (fontHeight / 2))
+  else
+    love.graphics.setFont(playBtnFont)
+    love.graphics.print("JOGANDO", 20, 20)
+  end
+
 end
